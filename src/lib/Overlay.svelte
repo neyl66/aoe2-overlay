d87cdd90feebd22926242afbd34beaff847d2010
Neyl66 <neyl.mage@seznam.cz>
2022-10-09 23:49:59 +0200

    Feat: redesign

--- src/lib/Overlay.svelte
+++ src/lib/Overlay.svelte
@@ -147,22 +147,23 @@
                         {/if}
 
                         <br>
-                        {player.name}
+                        <span class="player-name">{player.name}</span>
                         <br>
 
                         {#if (current_players[player.profile_id]) }
                             <span class="rating">{current_players[player.profile_id].rating} MMR</span>
 
-                            (#{current_players[player.profile_id].rank})
+                            <span class="rank">(#{current_players[player.profile_id].rank})</span>
                             <br>
 
                             <span class="win">{current_players[player.profile_id].wins}W</span>
                             <span class="loss">{current_players[player.profile_id].losses}L</span>
 
                             <br>
-                            <span class="winrate">{current_players[player.profile_id].winrate}%</span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="23" height="23" viewBox="0 0 24 24" class="winrate-icon"><path fill="#fff" d="M5 0c0 9.803 5.105 12.053 5.604 16h2.805c.497-3.947 5.591-6.197 5.591-16h-14zm7.006 14.62c-.408-.998-.969-1.959-1.548-2.953-1.422-2.438-3.011-5.162-3.379-9.667h9.842c-.368 4.506-1.953 7.23-3.372 9.669-.577.993-1.136 1.954-1.543 2.951zm-.006-3.073c-1.125-2.563-1.849-5.599-1.857-8.547h-1.383c.374 3.118 1.857 7.023 3.24 8.547zm12-9.547c-.372 4.105-2.808 8.091-6.873 9.438.297-.552.596-1.145.882-1.783 2.915-1.521 4.037-4.25 4.464-6.251h-2.688c.059-.45.103-.922.139-1.405h4.076zm-24 0c.372 4.105 2.808 8.091 6.873 9.438-.297-.552-.596-1.145-.882-1.783-2.915-1.521-4.037-4.25-4.464-6.251h2.688c-.058-.449-.102-.922-.138-1.404h-4.077zm13.438 15h-2.866c-.202 1.187-1.63 2.619-3.571 2.619v4.381h10v-4.381c-1.999 0-3.371-1.432-3.563-2.619zm2.562 6h-8v-2h8v2z"></path></svg>
-                            <br>
+                            <div class="winrate">
+                                <span>{current_players[player.profile_id].winrate}%</span>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" class="winrate-icon"><path fill="#fff" d="M5 0c0 9.803 5.105 12.053 5.604 16h2.805c.497-3.947 5.591-6.197 5.591-16h-14zm7.006 14.62c-.408-.998-.969-1.959-1.548-2.953-1.422-2.438-3.011-5.162-3.379-9.667h9.842c-.368 4.506-1.953 7.23-3.372 9.669-.577.993-1.136 1.954-1.543 2.951zm-.006-3.073c-1.125-2.563-1.849-5.599-1.857-8.547h-1.383c.374 3.118 1.857 7.023 3.24 8.547zm12-9.547c-.372 4.105-2.808 8.091-6.873 9.438.297-.552.596-1.145.882-1.783 2.915-1.521 4.037-4.25 4.464-6.251h-2.688c.059-.45.103-.922.139-1.405h4.076zm-24 0c.372 4.105 2.808 8.091 6.873 9.438-.297-.552-.596-1.145-.882-1.783-2.915-1.521-4.037-4.25-4.464-6.251h2.688c-.058-.449-.102-.922-.138-1.404h-4.077zm13.438 15h-2.866c-.202 1.187-1.63 2.619-3.571 2.619v4.381h10v-4.381c-1.999 0-3.371-1.432-3.563-2.619zm2.562 6h-8v-2h8v2z"></path></svg>
+                            </div>
                         {/if}
                     </div>
                 {/each}
@@ -189,7 +190,7 @@
         display: grid;
         grid-template-columns: repeat(2, 1fr);
         column-gap: 10px;
-        font-size: 21px;
+        font-size: 20px;
     }
 
     .player {
@@ -209,13 +210,22 @@
         border-radius: 50%;
     }
 
+    .player-name {
+        font-size: 1.2em;
+    }
+
     .rating {
         color: #D2AF26;
     }
 
-    .winrate, .winrate-icon {
-        display: inline-block;
-        vertical-align: middle;
+    .winrate {
+        display: flex;
+        align-items: center;
+        font-size: 0.88em;
+    }
+
+    .winrate-icon {
+        margin-left: 5px;
     }
 
     .win {
@@ -224,6 +234,7 @@
 
     .loss {
         color: rgb(239 68 68);
+        color: rgb(237 83 83);
     }
 </style>
 
