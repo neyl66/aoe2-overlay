093a051c263903303c49a658f9bd423ec3d69594
Neyl66 <neyl.mage@seznam.cz>
2023-03-19 03:26:01 +0100

    🚑️ Critical fix for websocket integration

--- src/lib/Overlay.svelte
+++ src/lib/Overlay.svelte
@@ -366,7 +366,7 @@
 
         <div class="match-info">
             <!-- Indicate queuing. -->
-            {#if (current_match?.type.toLowerCase() === "queuing")}
+            {#if (current_match?.type?.toLowerCase() === "queuing")}
                 <svg width="19" height="19" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="#fff" class="spinner"><path d="M12 1a11 11 0 1 0 11 11A11 11 0 0 0 12 1Zm0 19a8 8 0 1 1 8-8 8 8 0 0 1-8 8Z" opacity=".25"/><path d="M10.14 1.16a11 11 0 0 0-9 8.92A1.59 1.59 0 0 0 2.46 12a1.52 1.52 0 0 0 1.65-1.3 8 8 0 0 1 6.66-6.61A1.42 1.42 0 0 0 12 2.69a1.57 1.57 0 0 0-1.86-1.53Z" class="spinner-inner"/></svg>
                 {current_match.type.toUpperCase()}
                 |
